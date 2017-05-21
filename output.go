a407fc7f06272f9db0cbab3b0be918d1192a02ac
Scott Pakin <scott+gpg@pakin.org>
2017-05-20 23:22:43 -0600

    Split OutputResults into a few smaller functions

--- output.go
+++ output.go
@@ -8,41 +8,8 @@ import (
 	"io"
 )
 
-// OutputResults is the program's top-level output routine.  It outputs a
-// variety of information about frustration within a graph.
-func OutputResults(w io.Writer, g Graph, ecs [][][2]string) {
-	// Convert the edges back to paths for a more readable presentation.
-	// Determine which paths are frustrated cycles.
-	ps := make([][]string, len(ecs))
-	isFrust := make([]bool, len(ecs))
-	for i, ec := range ecs {
-		ps[i] = g.edgesToPath(ec)
-		isFrust[i] = g.isFrustrated(ps[i])
-	}
-
-	// Output each cycle preceded by whether it is frustrated or not.  As
-	// we go along, keep track of all vertices that appear within a
-	// frustrated cycle, and tally the number of frustrated cycles
-	// encountered.
-	fvs := make(map[string]Empty, len(g.Vs))
-	nfcs := 0 // Number of frustrated cycles
-	for i, p := range ps {
-		f := isFrust[i]
-		if f {
-			fmt.Fprintf(w, "FC  ")
-			nfcs++
-		} else {
-			fmt.Fprintf(w, "NFC ")
-		}
-		for _, v := range p {
-			fmt.Fprintf(w, " %s", v)
-			if f {
-				fvs[v] = Empty{}
-			}
-		}
-		fmt.Fprintln(w, "")
-	}
-
+// outputVertices outputs all vertices, categorized and tallied.
+func outputVertices(w io.Writer, g Graph, ps [][]string, isFrust []bool) {
 	// Tally the number of times each vertex appears in a frustrated cycle
 	// and in a non-frustrated cycle.
 	fVerts := make(map[string]int)
@@ -72,6 +39,12 @@ func OutputResults(w io.Writer, g Graph, ecs [][][2]string) {
 		}
 	}
 
+	// Output some summary statistics.
+	fmt.Fprintf(w, "#FV  %d / %d = %f\n", nfvs, len(g.Vs), float64(nfvs)/float64(len(g.Vs)))
+}
+
+// outputEdges outputs all edges, categorized and tallied.
+func outputEdges(w io.Writer, g Graph, ps [][]string, isFrust []bool) {
 	// Tally the number of times each edge appears in a frustrated cycle
 	// and in a non-frustrated cycle.
 	fEdges := make(map[[2]string]int)
@@ -105,8 +78,51 @@ func OutputResults(w io.Writer, g Graph, ecs [][][2]string) {
 		}
 	}
 
-	// Output some cycle, edge, and vertex statistics.
-	fmt.Fprintf(w, "#FC  %d / %d = %f\n", nfcs, len(ps), float64(nfcs)/float64(len(ps)))
+	// Output some summary statistics.
 	fmt.Fprintf(w, "#FE  %d / %d = %f\n", nfes, len(g.Es), float64(nfes)/float64(len(g.Es)))
-	fmt.Fprintf(w, "#FV  %d / %d = %f\n", nfvs, len(g.Vs), float64(nfvs)/float64(len(g.Vs)))
+}
+
+// outputCycles outputs all cycles, categorized and tallied.
+func outputCycles(w io.Writer, g Graph, ps [][]string, isFrust []bool) {
+	// Output each cycle preceded by whether it is frustrated or not.  As
+	// we go along, tally the number of frustrated cycles encountered.
+	fvs := make(map[string]Empty, len(g.Vs))
+	nfcs := 0 // Number of frustrated cycles
+	for i, p := range ps {
+		f := isFrust[i]
+		if f {
+			fmt.Fprintf(w, "FC  ")
+			nfcs++
+		} else {
+			fmt.Fprintf(w, "NFC ")
+		}
+		for _, v := range p {
+			fmt.Fprintf(w, " %s", v)
+			if f {
+				fvs[v] = Empty{}
+			}
+		}
+		fmt.Fprintln(w, "")
+	}
+
+	// Output some summary statistics.
+	fmt.Fprintf(w, "#FC  %d / %d = %f\n", nfcs, len(ps), float64(nfcs)/float64(len(ps)))
+}
+
+// OutputResults is the program's top-level output routine.  It outputs a
+// variety of information about frustration within a graph.
+func OutputResults(w io.Writer, g Graph, ecs [][][2]string) {
+	// Convert the edges back to paths for a more readable presentation.
+	// Determine which paths are frustrated cycles.
+	ps := make([][]string, len(ecs))
+	isFrust := make([]bool, len(ecs))
+	for i, ec := range ecs {
+		ps[i] = g.edgesToPath(ec)
+		isFrust[i] = g.isFrustrated(ps[i])
+	}
+
+	// Output information about the graph's vertices, edges, and cycles.
+	outputVertices(w, g, ps, isFrust)
+	outputEdges(w, g, ps, isFrust)
+	outputCycles(w, g, ps, isFrust)
 }
